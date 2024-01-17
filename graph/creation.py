03833326eca623494fd3daa304ea99c89432ea67
geomin76 <geom@vt.edu>
2024-01-17 08:55:50 -0600

    Adding all genre data

--- graph/creation.py
+++ graph/creation.py
@@ -22,10 +22,13 @@ token = None
 token_time = datetime.now()
 # seed genres that Spotify uses for /recommendation endpoint
 
-# 1/15/24 - Stopped after Dub. Run at Dubstep
+# 1/15/24 - Stopped after Dub. Run at Dubstep, starting at index 30
+# 1/16/24 - Stopped during Gospel. starting at index 41
+# 1/16/24 - Stopped during sleep
+# 1/17/24 - Done
 genres = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]
 try:
-    for INDEX in range(1, len(genres)):
+    for INDEX in range(len(genres)):
         token, token_time = get_token(token, token_time)
         print("Got token!")
         time.sleep(15)
