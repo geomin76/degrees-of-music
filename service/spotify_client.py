cea10f89c6dacda7568d6944b06dff168e6bcff8
Geo Min <31418725+geomin76@users.noreply.github.com>
2024-02-14 14:44:30 -0600

    Adding final code (#12)
    * Adding final code
    
    * Cleanup

--- service/spotify_client.py
+++ service/spotify_client.py
@@ -1,5 +1,10 @@
 import spotipy
 from flask import redirect
+import time
+import random
+from urllib.request import urlopen
+import base64
+import datetime
 
 def spotify_manager(session):
     cache_handler = spotipy.cache_handler.CacheFileHandler(session)
@@ -33,13 +38,46 @@ def retrieve_user_genres(spotify):
         genres.update(artist_data["genres"])
     return genres
 
-def create_playlist(spotify, song_list, bfs_genres):
-    # create playlist with songs provided
-    # add genres to description
-    # add songs to playlist!
-    return ""
+def create_playlist(spotify, bfs_genres):
+    description = "This playlist was created with these genres {}!".format(", ".join(bfs_genres), )
+    created_playlist = spotify.user_playlist_create(spotify.me()["id"], name="Degrees of Music playlist 🤠", description=description)
+    spotify.playlist_upload_cover_image(created_playlist["id"], base64_img())
+    return created_playlist["id"]
 
-def search_genre_data(spotify, genres):
-    # with time.sleep so no overload, but retrieve songs randomly from playlists from genres
-    # and generate all songs and return
-    return ""
+def search_genre_data(spotify, genres, playlist_id):
+    for index in range(0, len(genres), 5):
+        song_list = set()
+        for count in range(index, index + 5):
+            time.sleep(5)
+            playlists = spotify.search(type="playlist", q=genres[count], limit=3)
+            for playlist in playlists["playlists"]["items"]:
+                if "picked just for you" in playlist["description"] and playlist["owner"]["display_name"] == "Spotify":
+                    tracks = spotify.playlist_tracks(playlist["id"], "items(track(id))", 25)["items"]
+                    for track in random.sample(tracks, 15):
+                        song_list.add("spotify:track:{}".format(track["track"]["id"]))
+                    break
+                elif playlist["owner"]["display_name"] == "The Sounds of Spotify":
+                    tracks = spotify.playlist_tracks(playlist["id"], "items(track(id))", 25)["items"]
+                    for track in random.sample(tracks, 15):
+                        song_list.add("spotify:track:{}".format(track["track"]["id"]))
+                    break
+                else:
+                    tracks = spotify.playlist_tracks(playlists["playlists"]["items"][0]["id"], "items(track(id))", 20)["items"]
+                    if len(tracks) < 15:
+                        for inner_index in range(len(tracks)):
+                            song_list.add("spotify:track:{}".format(tracks[inner_index]["track"]["id"]))
+                    else:
+                        for track in random.sample(tracks, 15):
+                            song_list.add("spotify:track:{}".format(track["track"]["id"]))
+                    break
+
+        song_list = list(song_list)
+        random.shuffle(song_list)
+        spotify.playlist_add_items(playlist_id, song_list)
+
+def base64_img():
+    URL = 'https://senior-pics.s3.amazonaws.com/cap.jpg'
+    with urlopen(URL) as url:
+        f = url.read()
+        image = base64.b64encode(f).decode("ascii")
+        return image
