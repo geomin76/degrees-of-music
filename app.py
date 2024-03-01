8c555bb4ae473565a7f006788feceaec66e34058
geomin76 <geom@vt.edu>
2024-03-01 12:37:21 -0600

    Fixing sessions

--- app.py
+++ app.py
@@ -1,36 +1,31 @@
 import os
 from flask import Flask, session, request, redirect, render_template
-from flask_session import Session
 import spotipy
 from dotenv import load_dotenv
 from service.graph_client import bfs
-from service.spotify_client import retrieve_user_data, retrieve_user_genres, spotify_manager, search_genre_data, create_playlist
+from service.spotify_client import retrieve_user_data, retrieve_user_genres, search_genre_data, create_playlist
 import threading
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = './.flask_session/'
 
-Session(app)
 load_dotenv()
 
 @app.get("/")
-def login():
+def main():
     scope = "user-top-read playlist-read-private user-read-private user-read-email ugc-image-upload playlist-modify-public playlist-modify-private"
-    cache_handler = spotipy.cache_handler.CacheFileHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope,
-                                               cache_handler=cache_handler,
                                                show_dialog=True)
         
     # 2. if user has logged in, redirect and cache token
     if request.args.get("code"):
-        auth_manager.get_access_token(request.args.get("code"))
-        return redirect('/login')
+        session['tokens'] = {
+            'access_token': auth_manager.get_access_token(request.args.get("code"))["access_token"]
+        }
+        return redirect('/')
     
     # 1. if user has not logged in yet
-    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+    if 'tokens' not in session:
         auth_url = auth_manager.get_authorize_url()
         return render_template("login.html", auth_url=auth_url)
 
@@ -39,7 +34,7 @@ def login():
 
 @app.get("/index")
 def index():
-    spotify = spotify_manager(session)
+    spotify = spotipy.Spotify(auth=session['tokens'].get('access_token'))
     data = retrieve_user_data(spotify)
 
     return render_template("index.html", name=spotify.me()["display_name"], data=data)
@@ -48,7 +43,7 @@ def index():
 @app.post("/search")
 def search():
     depth = int(request.form['depth'])
-    spotify = spotify_manager(session)
+    spotify = spotipy.Spotify(auth=session['tokens'].get('access_token'))
     
     def threaded_function(depth):
         genres = list(retrieve_user_genres(spotify))
