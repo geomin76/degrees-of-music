fb31ba56e6fb5bc58d4cc8e6d163e2a7f5fa4e06
Geo Min <31418725+geomin76@users.noreply.github.com>
2024-03-01 11:53:33 -0600

    Initial frontend (#15)
    * Adding initial frontend
    
    * Adding more html

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-from flask import Flask, session, request, redirect
+from flask import Flask, session, request, redirect, render_template
 from flask_session import Session
 import spotipy
 from dotenv import load_dotenv
 from service.graph_client import bfs
 from service.spotify_client import retrieve_user_data, retrieve_user_genres, spotify_manager, search_genre_data, create_playlist
 import threading
-import time
 
 app = Flask(__name__)
 app = Flask(__name__)
@@ -18,17 +17,13 @@ Session(app)
 load_dotenv()
 
 @app.get("/")
-def read_root():
-    return "Hello, World!"
-
-@app.get("/login")
 def login():
     scope = "user-top-read playlist-read-private user-read-private user-read-email ugc-image-upload playlist-modify-public playlist-modify-private"
     cache_handler = spotipy.cache_handler.CacheFileHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope,
                                                cache_handler=cache_handler,
                                                show_dialog=True)
-    
+        
     # 2. if user has logged in, redirect and cache token
     if request.args.get("code"):
         auth_manager.get_access_token(request.args.get("code"))
@@ -37,19 +32,22 @@ def login():
     # 1. if user has not logged in yet
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         auth_url = auth_manager.get_authorize_url()
-        return {"auth_url": auth_url, "redirect": ""}
+        return render_template("login.html", auth_url=auth_url)
 
     # 3. user has been authenticated and redirected to proper URL
-    return {"auth_url": "", "redirect": "success"}
+    return redirect("/index")
 
-@app.get("/user-data")
-def user_data():
+@app.get("/index")
+def index():
     spotify = spotify_manager(session)
-    return retrieve_user_data(spotify)
+    data = retrieve_user_data(spotify)
 
-@app.get("/search")
+    return render_template("index.html", name=spotify.me()["display_name"], data=data)
+
+
+@app.post("/search")
 def search():
-    depth = int(request.args.get('depth'))
+    depth = int(request.form['depth'])
     spotify = spotify_manager(session)
     
     def threaded_function(depth):
@@ -60,12 +58,15 @@ def search():
 
     thread = threading.Thread(target=threaded_function, kwargs={'depth': depth})
     thread.start()
+    time = ""
     if depth == 1:
-        return "1 - 2 minutes"
+        time = "1 - 2 minutes"
     elif depth == 2:
-        return "2 - 3 minutes"
+        time = "2 - 3 minutes"
     elif depth == 3: 
-        return "3 - 5 minutes"
+        time = "3 - 5 minutes"
+    
+    return render_template("submit.html", time=time)
 
 if __name__ == "__main__":
     app.run(debug=True)
